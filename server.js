db5ea5861129204370e11a3a4de3afdb49c8e816
x4whlt <steconx4whlt@gmail.com>
2026-04-08 22:54:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -57,11 +57,48 @@ app.get("/playlist/:username.m3u", (req, res) => {
     let playlist = "#EXTM3U\n\n";
 
     const channels = [
-        {
-            name: "LIVE VIP",
-            url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-        }
-    ];
+const channels = [
+    {
+        name: "3HD",
+        url: "https://ch3-33-web.cdn.byteark.com/live/playlist_720p/index.m3u8"
+    },
+    {
+        name: "CH5",
+        url: "https://639bc5877c5fe.streamlock.net/tv5hdlive/tv5hdlive/playlist.m3u8"
+    },
+    {
+        name: "7HD",
+        url: "https://live-cdn.ch7.com/out/v1/eafeb02c55b64a15b278b1e66c7fc776/playlist_9.m3u8"
+    },
+    {
+        name: "Mono29",
+        url: "https://monomax-uiripn.cdn.byteark.com/plain/th/1080p/index.m3u8"
+    },
+    {
+        name: "Thai PBS",
+        url: "http://thaipbs-live.cdn.byteark.com/live/playlist_1080p/index.m3u8"
+    },
+    {
+        name: "Workpoint",
+        url: "https://global-media.sooplive.com/live/workpoint/master.m3u8"
+    },
+    {
+        name: "GMM25",
+        url: "https://bcovlive-a.akamaihd.net/57d4bf695e80436d9335f4f50adbe438/ap-southeast-1/6415628290001/7e85dc4a59904e45b4fdffebd62e1d82/playlist_ssaiM.m3u8"
+    },
+    {
+        name: "ONE31",
+        url: "https://bcovlive-a.akamaihd.net/b6603a14ea59440a95e9235e14bc9332/ap-southeast-1/6415628290001/9c3d7fc7d10840a69e48b5939ae886e0/playlist_ssaiM.m3u8"
+    },
+    {
+        name: "TopNews",
+        url: "https://live.topnews.co.th/hls/topnews_a_FHD1080.m3u8"
+    },
+    {
+        name: "TNN16",
+        url: "https://aisplay.ais.co.th/portal/live?vid=59671d1cd817de1df19711a6"
+    }
+];
 
     channels.forEach(ch => {
         const token = crypto.randomBytes(16).toString("hex");
